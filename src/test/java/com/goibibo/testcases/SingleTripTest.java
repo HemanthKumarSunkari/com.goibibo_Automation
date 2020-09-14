d047c3236f799e82de7d36308da35c82858e2261
HemanthKumarSunkari <hemanth.monk@gmail.com>
2020-09-14 22:23:12 +0530

    updated xml file and added invocation count to testcase

--- src/test/java/com/goibibo/testcases/SingleTripTest.java
+++ src/test/java/com/goibibo/testcases/SingleTripTest.java
@@ -14,7 +14,7 @@ import com.goibibo.util.TestUtil;
 public class SingleTripTest extends TestBase {
 
 	
-	@Test()
+	@Test(invocationCount = 2)
 	public static void SingleTripPaymentTest() throws ParseException {
 		LandingPage lp = new LandingPage();
 		
